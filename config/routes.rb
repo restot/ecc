fbf62e976dc51988e77a0a39a9cda94cb8c2d47a
restot <92ilya.icom@gmail.com>
2017-02-20 18:03:12 -0500

    routes fix Mon Feb 20 18:03:12 EST 2017

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 get 'stylish_portfolios/index'
 get 'creatives/index'
 get 'freelancers/index'
-root 'clean_blogs#index'
-get  'about'    => 'clean_blogs#about'
-get  'post'     => 'clean_blogs#post'
-get  'contact'  => 'clean_blogs#contact'
+get 'clean_blogs/index'
+#get  'about'    => 'clean_blogs#about'
+#get  'post'     => 'clean_blogs#post'
+#get  'contact'  => 'clean_blogs#contact'
 get 'grayscales/index'
+
+root 'creatives#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
